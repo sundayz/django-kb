d8fe2876e0068a7ce203b379ff5c8ad2fecb8b01
Sundays <sundays@wagn.me>
2018-03-07 20:48:54 +0000

    add form validation for search. todo: messages for bad queries or no results

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,8 @@ from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 
 from django.shortcuts import redirect
+
+from polls.forms import SearchForm
 from .models import Article, Category, Comment, UserProfile
 
 from django.urls import reverse
@@ -28,13 +30,18 @@ def detail(request, article_id):
 
 def search(request):
     # GET request handling
-    if request.method == 'GET':
-        search = request.GET.get('search', None)
-        if search is None or search == "":
-            return redirect("index")
-    # CONTINUE
-    article_results = Article.objects.filter(name__icontains=search)
-    # ask sam ^^^^^
-    comment_results = Comment.objects.filter(comment__icontains=search)
-    context = {'article_results': article_results, 'comment_results': comment_results, 'search': search}
-    return render(request, 'polls/search.html', context)
+    query = request.GET.get('search')
+    if query is not None:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            clean_query = form.cleaned_data['search']
+            articles = Article.objects.filter(name__icontains=clean_query)
+            comments = Comment.objects.filter(comment__icontains=clean_query)
+            context = {'articles': articles, 'comments': comments, 'search': clean_query}
+            return render(request, 'polls/search.html', context)
+        else:
+            # TODO: messages
+            return HttpResponseRedirect('/')
+    else:
+        # TODO: messages
+        return HttpResponseRedirect('/')
